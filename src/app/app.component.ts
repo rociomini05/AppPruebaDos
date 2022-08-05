062d5e30443aa06f47c537414f6763ff411d7faf
Rocio Mini <rociomini25@gmail.com>
2022-08-05 08:49:52 -0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  titulos: string [];
+ campoTexto: string ;
+ persona: any;
+
  constructor (){
   this.titulos = ['Jurassic Park', 'Star Wars', 'Jumanji']
+  this.campoTexto = 'Valor Inicial'
+  this.persona = {}
  }
 
  onAlertaPulsada(){
   console.log('Pulsada!!!');
+ }
+ onClick(){
+  this.campoTexto= 'Valor desde el boton'
  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './saludo/saludo.component';
 import { PadreComponent } from './padre/padre.component';
@@ -18,7 +18,8 @@ import { AlertaComponent } from './alerta/alerta.component';
     AlertaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
